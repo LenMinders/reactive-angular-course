40dd708cdabcfc611819d56a680f1d996ac6e4ef
Len Minders <len.minders@standardbank.co.za>
2021-06-15 11:36:45 +0200

    save method

--- src/app/course-card-list/course-card-list.component.ts
+++ src/app/course-card-list/course-card-list.component.ts
@@ -1,7 +1,9 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnInit, Output } from '@angular/core';
 import { Course } from '../model/course';
 import {MatDialog, MatDialogConfig} from '@angular/material/dialog';
 import {CourseDialogComponent} from '../course-dialog/course-dialog.component';
+import { EventEmitter } from 'events';
+import { filter, tap } from 'rxjs/operators';
 
 @Component({
   selector: 'course-card-list',
@@ -12,6 +14,9 @@ export class CourseCardListComponent implements OnInit {
 
   @Input()
   courses: Course[] = [];
+
+  @Output()
+  private coursesChanged = new EventEmitter();
 
   constructor(private dialog: MatDialog) { }
 
@@ -29,6 +34,15 @@ export class CourseCardListComponent implements OnInit {
     dialogConfig.data = course;
 
     const dialogRef = this.dialog.open(CourseDialogComponent, dialogConfig);
+
+    dialogRef.afterClosed()
+      .pipe(
+        filter(val => !!val),
+        tap(() => this.coursesChanged.emit())
+      )
+      .subscribe(
+
+      );
 
   }
 
